52b8fee5dc6de8d914686fe46f9bbef252caeb07
royye6 <royye6@gmail.com>
2024-07-26 17:20:44 +0200

    fix: remove html elements from description

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -31,6 +32,10 @@ type Recipe struct {
 	RecipeScore float64  `json:"spoonacularScore"`
 }
 
+type Trivia struct {
+	Text string `json:"text"`
+}
+
 func getUserInput(prompt string) string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print(prompt)
@@ -40,9 +45,8 @@ func getUserInput(prompt string) string {
 }
 
 func cleanDescription(htmlText string) string {
-	cleanText := strings.ReplaceAll(htmlText, "<[^>]*", "")
-	cleanText = strings.TrimSpace(cleanText)
-
+	re := regexp.MustCompile("<.*?>")
+	cleanText := re.ReplaceAllString(htmlText, "")
 	return cleanText
 }
 
@@ -56,7 +60,6 @@ func getData(c string, i string) (string, error) {
 	category, ingredient := c, i
 
 	url := fmt.Sprintf("https://api.spoonacular.com/recipes/complexSearch?apiKey=%s&type=%s&query=%s&addRecipeInformation=True&number=1&sort=random", apiKey, category, ingredient)
-
 	response, err := http.Get(url)
 
 	if err != nil {
@@ -80,7 +83,7 @@ func getData(c string, i string) (string, error) {
 		randRecipe := apiResponse.Results[0]
 		cleanDescription := cleanDescription(randRecipe.Description)
 
-		recipeDetails := fmt.Sprintf("\n\n\nTitle: %s\n\nPrep Time (minutes): %d\n\nServings: %d\n\nSource URL: %s\n\nImage: %s\n\nDescription:\n\n%s\n\nRecipe Score: %.2f\n",
+		recipeDetails := fmt.Sprintf("*****\n\nTitle: %s\n\nPrep Time (minutes): %d\n\nServings: %d\n\nSource URL: %s\n\nImage: %s\n\nDescription:\n\n%s\n\nRecipe Score: %.2f\n",
 			randRecipe.Title, *randRecipe.PrepTime, *randRecipe.Servings, randRecipe.SourceUrl, randRecipe.Image, cleanDescription, randRecipe.RecipeScore)
 
 		return recipeDetails, nil
@@ -90,9 +93,62 @@ func getData(c string, i string) (string, error) {
 
 }
 
+func saveData(r string) error {
+	file, err := os.OpenFile("recipe.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("error opening file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(r + "\n\n")
+	if err != nil {
+		return fmt.Errorf("error writing to file: %w", err)
+	}
+	return nil
+}
+
+func getFoodFact() (string, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	apiKey := os.Getenv("SPOONACULAR_API_KEY")
+
+	url := fmt.Sprintf("https://api.spoonacular.com/food/trivia/random?apiKey=%s", apiKey)
+	response, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusOK {
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			return "", fmt.Errorf("error reading response body: %v", err)
+		}
+
+		var foodFact Trivia
+		err = json.Unmarshal(body, &foodFact)
+
+		if err != nil {
+			return "", fmt.Errorf("error unmarshalling(deserializing) JSON response: %v", err)
+		}
+
+		fact := foodFact.Text
+
+		randFact := fmt.Sprintf("\n%s\n", fact)
+
+		return randFact, nil
+	}
+
+	return "", fmt.Errorf("error fetching trivia from URL: %s", url)
+}
+
 func main() {
 	for {
-		mainMenu := []string{
+		mainMenu := []string {
 			"Main menu:\n",
 			"1. Get a recipe",
 			"2. Learn a surprising food fact",
@@ -156,8 +212,26 @@ func main() {
 					time.Sleep(1 * time.Second)
 					fmt.Println(recipe)
 					time.Sleep(1 * time.Second)
+					for {
+						save_data := strings.TrimSpace(getUserInput("\nDo you want to save this recipe to recipe.txt? y/n: "))
+						switch save_data {
+						case "y":
+							saveData(recipe)
+							time.Sleep(1 * time.Second)
+							fmt.Println("Recipe saved successfully...")
+							fmt.Println("\nReturned to main menu\n ")
+							time.Sleep(1 * time.Second)
+							main()
 
-					return
+							return
+						case "n":
+							fmt.Println("Exiting...")
+							time.Sleep(1 * time.Second)
+							return
+						default:
+							fmt.Println("\nInvalid choice")
+						}
+					}
 				case 2:
 					category := "Snack"
 					fmt.Printf("\nCategory: %s\n", category)
@@ -176,8 +250,26 @@ func main() {
 					time.Sleep(1 * time.Second)
 					fmt.Println(recipe)
 					time.Sleep(1 * time.Second)
+					for {
+						save_data := strings.TrimSpace(getUserInput("\nDo you want to save this recipe to recipe.txt? y/n: "))
+						switch save_data {
+						case "y":
+							saveData(recipe)
+							time.Sleep(1 * time.Second)
+							fmt.Println("Recipe saved successfully...")
+							fmt.Println("\nReturned to main menu\n ")
+							time.Sleep(1 * time.Second)
+							main()
 
-					return
+							return
+						case "n":
+							fmt.Println("Exiting...")
+							time.Sleep(1 * time.Second)
+							return
+						default:
+							fmt.Println("\nInvalid choice")
+						}
+					}
 				case 3:
 					category := "Salad"
 					fmt.Printf("\nCategory: %s\n", category)
@@ -196,8 +288,26 @@ func main() {
 					time.Sleep(1 * time.Second)
 					fmt.Println(recipe)
 					time.Sleep(1 * time.Second)
+					for {
+						save_data := strings.TrimSpace(getUserInput("\nDo you want to save this recipe to recipe.txt? y/n: "))
+						switch save_data {
+						case "y":
+							saveData(recipe)
+							time.Sleep(1 * time.Second)
+							fmt.Println("Recipe saved successfully...")
+							fmt.Println("\nReturned to main menu\n ")
+							time.Sleep(1 * time.Second)
+							main()
 
-					return
+							return
+						case "n":
+							fmt.Println("Exiting...")
+							time.Sleep(1 * time.Second)
+							return
+						default:
+							fmt.Println("\nInvalid choice")
+						}
+					}
 				case 4:
 					category := "Main Course"
 					fmt.Printf("\nCategory: %s\n", category)
@@ -216,8 +326,26 @@ func main() {
 					time.Sleep(1 * time.Second)
 					fmt.Println(recipe)
 					time.Sleep(1 * time.Second)
+					for {
+						save_data := strings.TrimSpace(getUserInput("\nDo you want to save this recipe to recipe.txt? y/n: "))
+						switch save_data {
+						case "y":
+							saveData(recipe)
+							time.Sleep(1 * time.Second)
+							fmt.Println("Recipe saved successfully...")
+							fmt.Println("\nReturned to main menu\n ")
+							time.Sleep(1 * time.Second)
+							main()
 
-					return
+							return
+						case "n":
+							fmt.Println("Exiting...")
+							time.Sleep(1 * time.Second)
+							return
+						default:
+							fmt.Println("\nInvalid choice")
+						}
+					}
 				case 5:
 					category := "Dessert"
 					fmt.Printf("\nCategory: %s\n", category)
@@ -236,8 +364,26 @@ func main() {
 					time.Sleep(1 * time.Second)
 					fmt.Println(recipe)
 					time.Sleep(1 * time.Second)
+					for {
+						save_data := strings.TrimSpace(getUserInput("\nDo you want to save this recipe to recipe.txt? y/n: "))
+						switch save_data {
+						case "y":
+							saveData(recipe)
+							time.Sleep(1 * time.Second)
+							fmt.Println("Recipe saved successfully...")
+							fmt.Println("\nReturned to main menu\n ")
+							time.Sleep(1 * time.Second)
+							main()
 
-					return
+							return
+						case "n":
+							fmt.Println("Exiting...")
+							time.Sleep(1 * time.Second)
+							return
+						default:
+							fmt.Println("\nInvalid choice")
+						}
+					}
 				case 6:
 					category := "Drink"
 					fmt.Printf("\nCategory: %s\n", category)
@@ -256,8 +402,26 @@ func main() {
 					time.Sleep(1 * time.Second)
 					fmt.Println(recipe)
 					time.Sleep(1 * time.Second)
+					for {
+						save_data := strings.TrimSpace(getUserInput("\nDo you want to save this recipe to recipe.txt? y/n: "))
+						switch save_data {
+						case "y":
+							saveData(recipe)
+							time.Sleep(1 * time.Second)
+							fmt.Println("Recipe saved successfully...")
+							fmt.Println("\nReturned to main menu\n ")
+							time.Sleep(1 * time.Second)
+							main()
 
-					return
+							return
+						case "n":
+							fmt.Println("Exiting...")
+							time.Sleep(1 * time.Second)
+							return
+						default:
+							fmt.Println("\nInvalid choice")
+						}
+					}
 				case 7:
 					main()
 				default:
@@ -265,9 +429,17 @@ func main() {
 				}
 			}
 		case 2:
-			fmt.Println("Option 2")
+			trivia, err := getFoodFact()
+			if err != nil {
+				fmt.Println("error fetching trivia")
+				return
+			}
+			fmt.Printf(trivia + "\n")
+			time.Sleep(3 * time.Second)
+			return
 		case 3:
 			fmt.Println("Exiting...")
+			time.Sleep(1 * time.Second)
 			return
 		default:
 			fmt.Println("\nInvalid selection. Enter 1, 2 or 3.")
